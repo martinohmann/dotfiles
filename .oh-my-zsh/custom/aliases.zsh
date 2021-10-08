122247f8654d0f391a4cbf8df2e669780a9b28de
Martin Ohmann <martin.ohmann@bonial.com>
2021-10-08 08:41:02 +0200

    .oh-my-zsh: only create ls_color aliases on linux

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -14,10 +14,12 @@ alias lsd='ls -d */'
 
 # List direcory contents
 # alias l='ls -lah'
-alias ls='ls -F --color=auto'
-alias l='ls_color -ah'
-alias ll='ls_color'
-alias la='ls_color -a'
+if [ "$(uname)" = Linux ]; then
+  alias ls='ls -F --color=auto'
+  alias l='ls_color -ah'
+  alias ll='ls_color'
+  alias la='ls_color -a'
+fi
 # alias lg='ls_color -a --group-directories-first'
 # alias sl=ls # often screw this up
 # alias lso="ls -lG | awk '{k=0;for(i=0;i<=8;i++)k+=((substr(\$1,i+2,1)~/[rwx]/)*2^(8-i));if(k)printf(\" %0o \",k);print}'"
